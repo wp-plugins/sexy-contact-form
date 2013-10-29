2b33271624ad07e9f815bd4fb80d5e39f4b0ca0d
2glux <2glux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-29 00:13:00 +0000

    Modified javasctipt
    git-svn-id: https://plugins.svn.wordpress.org/sexy-contact-form/tags/0.9.2@795088 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/js/sexycontactform.js
+++ includes/js/sexycontactform.js
@@ -302,6 +302,11 @@ $(document).ready(function() {
 	$(".sexy_url").blur(function() {
 		validate_url($(this),false);
 	});
+	$(".sexy_url_reserve").blur(function() {
+		if($(this).hasClass('active')) {
+			$(this).addClass('passive');
+		}
+	});
 	
 	$('.sexycontactform_input_element input,.sexycontactform_input_element textarea').focus(function() {
 		$(this).parents('.sexycontactform_input_element').addClass('focused');
